c805d196bb74ea70a6224f081f2913731389cc56
peterpontocom <willpontocom07@gmail.com>
2022-06-01 08:58:12 +0200

    icones e js no login

--- scr/login.js
+++ scr/login.js
@@ -1,3 +1,15 @@
+
+/*
+    Left
+*/
+
+function showUpRemove() {
+    window.document.getElementsByClassName('.pontos').style.display = 'block'
+}
+
+/*
+    Right
+ */
 function show(){
     let pass = window.document.querySelector('#pass')
     let show = window.document.querySelector('.show')
@@ -14,4 +26,28 @@ function hide(){
     pass.setAttribute ('type', 'password')
     hide.style.display = 'none'
     show.style.display = 'inline-block'
+}
+
+function bordaN(){
+    let borda = document.querySelector('.input-file')
+    let Username = window.document.querySelector('#name')
+    let password = window.document.querySelector('#pass')
+    if(Username.value == 'Luntala Maria' || Username.value == 'Carla Sole'){
+        borda.style.borderBottom = '2px solid #13d644ea'
+    }
+
+    else if (Username.value == 0){
+        borda.style.borderBottom = "2px solid #2F2E41"
+    }
+
+    else{
+        borda.style.borderBottom = '2px solid #ff0062'
+    }
+}
+
+function BordaP(){
+    let borda = document.querySelector('.input-file')
+    let Username = window.document.querySelector('#name')
+    let password = window.document.querySelector('#pass')
+    
 }
